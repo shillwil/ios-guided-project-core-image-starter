812dd70d9653ab05ba9f58ac89d80f399952911a
Alex Shillingford <alex.shillingford@icloud.com>
2020-02-10 11:10:59 -0700

    Add filterImage function. Add originalImage variable and assign it the current image in the image view

--- PhotoFilter/PhotoFilter/PhotoFilterViewController.swift
+++ PhotoFilter/PhotoFilter/PhotoFilterViewController.swift
@@ -1,8 +1,13 @@
 import UIKit
 import CoreImage
+import CoreImage.CIFilterBuiltins
 import Photos
 
 class PhotoFilterViewController: UIViewController {
+    
+    private var context = CIContext(options: nil)
+    
+    private var originalImage: UIImage?
 
 	@IBOutlet var brightnessSlider: UISlider!
 	@IBOutlet var contrastSlider: UISlider!
@@ -11,8 +16,39 @@ class PhotoFilterViewController: UIViewController {
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-
+        
+        
+        originalImage = imageView.image
 	}
+    
+    func filterImage(_ image: UIImage) -> UIImage? {
+        // UIImage -> CGImage -> UIImage
+        
+        guard let cgImage = image.cgImage else { return nil }
+        
+        let ciImage = CIImage(cgImage: cgImage)
+        
+        let filter = CIFilter.colorControls() // Like a recipe
+        print(filter)
+        print(filter.attributes)
+        
+        filter.inputImage = ciImage
+        filter.brightness = brightnessSlider.value
+        filter.contrast = contrastSlider.value
+        filter.saturation = saturationSlider.value
+        
+        
+        // CIImage -> UIImage -> CIImage
+        guard let outputCIImage = filter.outputImage else { return nil }
+        
+        // Rendering the image (actually baking the cookies)
+        guard let outputCGImage = context.createCGImage(outputCIImage,
+                                                        from: CGRect(origin: .zero, size: image.size))
+                                                            else { return nil}
+        
+        return UIImage(cgImage: outputCGImage)
+    }
+    
 	
 	// MARK: Actions
 	
